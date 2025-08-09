0df02f215bedbfc6471e63555c8d2bb5da401523
lxl <pplxlee@126.com>
2025-08-09 11:30:58 +0800

    增加LanguageSwitcher组件

--- src/components/HeaderMenuBar.vue
+++ src/components/HeaderMenuBar.vue
@@ -8,18 +8,22 @@
             </template>
             <template #item="{ item, props }">
                 <div class="flex mr-4 ml-4 items-center">
-                <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
-                    <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                    <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+                        <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                            <span>{{ item.label }}</span>
+                        </a>
+                    </router-link>
+                    <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
                         <span>{{ item.label }}</span>
+                        <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
                     </a>
-                </router-link>
-                <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
-                    <span>{{ item.label }}</span>
-                    <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down" />
-                </a></div>
+                </div>
             </template>
             <template #end>
-                <ThemeSwitcher class="hidden-theme-switcher"/>
+                <div class="flex items-center">
+                    <ThemeSwitcher class="hidden-theme-switcher" />
+                    <LanguageSwitcher class="language-switcher" />
+                </div>
             </template>
         </Menubar>
     </div>
@@ -27,6 +31,9 @@
 
 <script setup>
 import { ref } from "vue";
+import LanguageSwitcher from './LanguageSwitcher.vue';
+import ThemeSwitcher from './ThemeSwitcher.vue';
+
 const title = ref('PR1');
 const items = ref([
     {
