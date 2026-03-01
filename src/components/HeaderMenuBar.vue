57a2881f983a51c636e6fd0e7f93bbb66aa42487
lxl <pplxlee@126.com>
2026-03-01 23:23:34 +0800

    py503

--- src/components/HeaderMenuBar.vue
+++ src/components/HeaderMenuBar.vue
@@ -37,7 +37,7 @@ import ThemeSwitcher from './ThemeSwitcher.vue';
 
 const { t } = useI18n();
 
-const title = ref('PY505');
+const title = ref('PY503');
 const items = computed(() => [
     {
         label: t('camera.base_settings'),
