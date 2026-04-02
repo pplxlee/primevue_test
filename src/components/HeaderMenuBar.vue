7bafe86d8fcdfc08255005e9908715b97ffe5faf
lxl <pplxlee@126.com>
2026-04-02 21:09:44 +0800

    PY503 Pro

--- src/components/HeaderMenuBar.vue
+++ src/components/HeaderMenuBar.vue
@@ -37,7 +37,7 @@ import ThemeSwitcher from './ThemeSwitcher.vue';
 
 const { t } = useI18n();
 
-const title = ref('PY503');
+const title = ref('PY503 Pro');
 const items = computed(() => [
     {
         label: t('camera.base_settings'),
